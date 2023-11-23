f115c3f88278171543db3034fdb0005989560a0e
Sean O'Hara <sohara@sohara.com>
2023-11-23 16:29:37 -0500

    Complete for-loops exercise using a 3x3 matrix

--- for-loops/src/main.rs
+++ for-loops/src/main.rs
@@ -1,3 +1,32 @@
+#![allow(clippy::needless_range_loop)]
+
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let mut transposed = [[0; 3]; 3];
+    for i in 0..3 {
+        for j in 0..3 {
+            transposed[j][i] = matrix[i][j];
+        }
+    }
+    transposed
+}
+
+fn pretty_print(matrix: &[[i32; 3]; 3]) {
+    for i in 0..3 {
+        println!(" {:?}", matrix[i])
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    println!("matrix:");
+    pretty_print(&matrix);
+
+    let transposed = transpose(matrix);
+    println!("transposed:");
+    pretty_print(&transposed);
 }
